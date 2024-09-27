fc576b8b282d9d5c563bfe88c5bfc340e4d34b25
tavolop3 <tavolop3>
2024-09-27 16:55:55 -0300

    config de ssh

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -26,9 +26,21 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [ 2222 ];
+    settings = {
+      PasswordAuthentication = true;
+      # AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      # UseDns = true;
+      # X11Forwarding = false;
+      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+  services.fail2ban.enable = true; #Warning: If you plan on using SSH on a public network, Fail2ban is highly recommended as a base standard of security.
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 2222 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
