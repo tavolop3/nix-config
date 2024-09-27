b5b262be945af704d119fb133877d3395e5890e2
tao <tavolop3>
2024-09-27 12:18:54 -0300

    subo config de desktop

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -5,7 +5,31 @@
     ./hardware-configuration.nix
   ];
 
-  networking.hostName = "laptop";
+  networking.hostName = "desktop";
 
-  # config especifica para laptop aca
+  # config especifica para desktop aca
+  # Bootloader.
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+      efiSupport = true;
+      useOSProber = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't wor>
+      device = "nodev";
+    };
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 }
